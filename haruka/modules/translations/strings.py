caf3aa3e41751d046c9f2eefbf8521fa2ae2e515
maulanaatthur <83605962+maulanaatthur@users.noreply.github.com>
2021-06-22 01:52:48 +0700

    Update strings.py

--- haruka/modules/translations/strings.py
+++ haruka/modules/translations/strings.py
@@ -1,9 +1,5 @@
 from haruka.modules.sql.translation import prev_locale
 from haruka.modules.translations.English import EnglishStrings
-from haruka.modules.translations.Russian import RussianStrings
-from haruka.modules.translations.Ukraine import UkrainianStrings
-from haruka.modules.translations.Spanish import SpanishStrings
-from haruka.modules.translations.Turkish import TurkishStrings
 from haruka.modules.translations.Indonesian import IndonesianStrings
 
 def tld(chat_id, t, show_none=True):
@@ -11,15 +7,7 @@ def tld(chat_id, t, show_none=True):
     print(chat_id, t)
     if LANGUAGE:
         LOCALE = LANGUAGE.locale_name
-        if LOCALE in ('ru') and t in RussianStrings:
-           return RussianStrings[t]
-        elif LOCALE in ('ua') and t in UkrainianStrings:
-            return UkrainianStrings[t]
-        elif LOCALE in ('es') and t in SpanishStrings:
-            return SpanishStrings[t]
-        elif LOCALE in ('tr') and t in TurkishStrings:
-            return TurkishStrings[t]
-        elif LOCALE in ('id') and t in IndonesianStrings:
+        if LOCALE in ('id') and t in IndonesianStrings:
             return IndonesianStrings[t]
         else:
             if t in EnglishStrings:
@@ -44,15 +32,7 @@ def tld_help(chat_id, t):
 
         print("Test2", t)
 
-        if LOCALE in ('ru') and t in RussianStrings:
-            return RussianStrings[t]
-        elif LOCALE in ('ua') and t in UkrainianStrings:
-            return UkrainianStrings[t]
-        elif LOCALE in ('es') and t in SpanishStrings:
-            return SpanishStrings[t]
-        elif LOCALE in ('tr') and t in TurkishStrings:
-            return TurkishStrings[t]
-        elif LOCALE in ('id') and t in IndonesianStrings:
+        if LOCALE in ('id') and t in IndonesianStrings:
             return IndonesianStrings[t]
         else:
             return False
